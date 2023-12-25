e5602bb6eeee797add310ee54bd1b50c4b00220c
Mert Aktas <mert_aktas08@hotmail.com>
2023-12-25 14:29:26 +0300

    2

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,14 +1,26 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using FormODV.Models;
+using System.Collections.Generic;
 
-namespace FormODV.Controllers;
-
-public class HomeController : Controller
+namespace FormODV.Controllers
 {
-    public IActionResult ShowMultipleViews()
+    public class HomeController : Controller
     {
-        return View("_Layout.cshtml");
+        public IActionResult Index()
+        {
+            var FormModel = new FormModel();
+            FormModel.FormNames.Add("Form 1");
+            FormModel.FormNames.Add("Form 2");
+            FormModel.FormNames.Add("Form 3");
+            FormModel.FormNames.Add("Form 4");
+            FormModel.FormNames.Add("Form 5");
+            FormModel.FormNames.Add("Form 6");
+            FormModel.FormNames.Add("Form 7");
+            FormModel.FormNames.Add("Form 8");
+            FormModel.FormNames.Add("Form 9");
+            FormModel.FormNames.Add("Form 10");
+            return View(FormModel);
+        }
     }
-
 }
