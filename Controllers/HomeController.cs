2399239dffc74faec5990eb51878ace6e0685f2c
Mert Aktas <mert_aktas08@hotmail.com>
2023-12-25 22:21:59 +0300

    3

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,17 +10,59 @@ namespace FormODV.Controllers
         public IActionResult Index()
         {
             var FormModel = new FormModel();
-            FormModel.FormNames.Add("Form 1");
-            FormModel.FormNames.Add("Form 2");
-            FormModel.FormNames.Add("Form 3");
-            FormModel.FormNames.Add("Form 4");
-            FormModel.FormNames.Add("Form 5");
-            FormModel.FormNames.Add("Form 6");
-            FormModel.FormNames.Add("Form 7");
-            FormModel.FormNames.Add("Form 8");
-            FormModel.FormNames.Add("Form 9");
-            FormModel.FormNames.Add("Form 10");
+            FormModel.FormNames.Add("Form_1");
+            FormModel.FormNames.Add("Form_2");
+            FormModel.FormNames.Add("Form_3");
+            FormModel.FormNames.Add("Form_4");
+            FormModel.FormNames.Add("Form_5");
+            FormModel.FormNames.Add("Form_6");
+            FormModel.FormNames.Add("Form_7");
+            FormModel.FormNames.Add("Form_8");
+            FormModel.FormNames.Add("Form_9");
+            FormModel.FormNames.Add("Form_10");
+
+
             return View(FormModel);
+        }
+        public IActionResult Form_1()
+        {
+            return View("Form1");
+        }
+        public IActionResult Form_2()
+        {
+            return View("Form2");
+        }
+        public IActionResult Form_3()
+        {
+            return View("Form3");
+        }
+        public IActionResult Form_4()
+        {
+            return View("Form4");
+        }
+        public IActionResult Form_5()
+        {
+            return View("Form5");
+        }
+        public IActionResult Form_6()
+        {
+            return View("Form6");
+        }
+        public IActionResult Form_7()
+        {
+            return View("Form7");
+        }
+        public IActionResult Form_8()
+        {
+            return View("Form8");
+        }
+        public IActionResult Form_9()
+        {
+            return View("Form9");
+        }
+        public IActionResult Form_10()
+        {
+            return View("Form10");
         }
     }
 }
