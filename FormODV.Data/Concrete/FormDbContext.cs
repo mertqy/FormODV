0e7214a8bec5a94ce0ef35a8f165d88baecc44d4
Mert Aktas <mert_aktas08@hotmail.com>
2023-12-26 21:59:40 +0300

    Add initial data for forms

--- FormODV.Data/Concrete/FormDbContext.cs
+++ FormODV.Data/Concrete/FormDbContext.cs
@@ -16,7 +16,16 @@ public class FormDbContext : DbContext
 
         modelBuilder.Entity<Form>().HasData(
             new List<Form>(){
-                new Form
+                new (){Id = 1,Name = "Form_1,"},
+                new (){Id = 2,Name = "Form_2,"},
+                new (){Id = 3,Name = "Form_3,"},
+                new (){Id = 4,Name = "Form_4,"},
+                new (){Id = 5,Name = "Form_5,"},
+                new (){Id = 6,Name = "Form_6,"},
+                new (){Id = 7,Name = "Form_7,"},
+                new (){Id = 8,Name = "Form_8,"},
+                new (){Id = 9,Name = "Form_9,"},
+                new (){Id = 10,Name = "Form_10,"},
             }
         );
     }
